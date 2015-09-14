786ca99fd5cfc58f0b1ac1a0889ffd06df9dacfb
Austin Heiman <atheimanksu@gmail.com>
2015-09-13 22:48:52 -0500

    all functionality created in google-drive-data-file.js, untested

--- extension/assets/js/google-drive-data-file.js
+++ extension/assets/js/google-drive-data-file.js
@@ -1,5 +1,5 @@
 (function () {
-  angular.module('gDriveDataFile')
+  angular.module('gDriveDataFileModule', [])
 
   .config(function ($httpProvider) {
     chrome.identity.getAuthToken({interactive: true}, function (token) {
@@ -10,33 +10,37 @@
   .service('gDriveDataFile', ['$http', function ($http) {
     var gDriveBaseURL = 'https://www.googleapis.com/drive/v2/files';
     var gDriveUploadURL = 'https://www.googleapis.com/upload/drive/v2/files/';
+    var dataFileIdKey = '_dataFileId';
+    var appfolderIdKey = '_appfolderId';
+
     var respHandler = function (resp) {
-      console.log('Resp received:', resp);
+      console.info('Resp received:', resp);
     }
     var errRespHandler = function (resp) {
-      console.log('Error resp received:', resp);
+      console.error('Error resp received:', resp);
     };
 
     var _gddf = this;
 
     this._getAppfolderId = function () {
-      var appfolderIdKey = '_appfolderId';
+      return 'appfolder';
+      // var appfolderIdKey = '_appfolderId';
 
-      if (typeof _gddf[appfolderIdKey] !== 'undefined') {
-        // check if appfolder id is cached
-        return _gddf[appfolderIdKey];
-      } else {
-        $http.get(gDriveBaseURL + '/appfolder')
-        .then(function (resp) {
-          respHandler.call(this, resp);
-          // cache appfolder id
-          _gddf[appfolderIdKey] = resp['data']['id'];
-        }, function (errResp) {
-          errRespHandler.call(this, errResp);
-        })
-        // don't wait for appfolder metadata resp (g api will redirect)
-        return 'appfolder';
-      }
+      // if (typeof _gddf[appfolderIdKey] !== 'undefined') {
+      //   // check if appfolder id is cached
+      //   return _gddf[appfolderIdKey];
+      // } else {
+      //   $http.get(gDriveBaseURL + '/appfolder')
+      //   .then(function (resp) {
+      //     respHandler.call(this, resp);
+      //     // cache appfolder id
+      //     _gddf[appfolderIdKey] = resp['data']['id'];
+      //   }, function (errResp) {
+      //     errRespHandler.call(this, errResp);
+      //   })
+      //   // don't wait for appfolder metadata resp (g api will redirect)
+      //   return 'appfolder';
+      // }
     }
 
     this._createDataFile = function () {
@@ -49,13 +53,14 @@
       return $http.post(gDriveBaseURL, metadata);
     };
 
-    this._getData = function (callback) {
-      // callback accepts data (obj) which is raw obj stored in g drive
+    this.getData = function (callback) {
+      // callback (optional) accepts data (obj) stored in g drive
       _gddf._getDataFileId(function (dataFileId) {
         $http.get(gDriveBaseURL + '/' + dataFileId + '?alt=media')
         .then(function (resp) {
           respHandler.call(this, resp);
-          callback.call(this, resp['data']);
+          if (typeof callback !== 'undefined')
+            callback.call(this, resp['data']);
         }, function (errResp) {
           errRespHandler.call(this, errResp);
         })
@@ -64,7 +69,6 @@
 
     this._getDataFileId = function (callback) {
       // - callback accepts data file id (string)
-      var dataFileIdKey = '_dataFileId';
 
       if (typeof _gddf['_dataFileId'] !== 'undefined') {
         // check if data file id is cached
@@ -80,14 +84,14 @@
             // execute callback
             callback.call(this, _gddf[dataFileIdKey]);
           } else {
-            // create data file
+            // data file does not exist, create data file
             _gddf._createDataFile()
             .then(function (createResp) {
               respHandler.call(this, createResp);
               // cache data file id
               _gddf[dataFileIdKey] = createResp['data']['id'];
               // execute callback
-              callback.call(_gddf[dataFileIdKey]);
+              callback.call(this, _gddf[dataFileIdKey]);
             }, function (errResp) {
               // if errResp from creating data file
               errRespHandler.call(this, errResp);
@@ -100,20 +104,39 @@
       }
     };
 
-    this._update = function (data, callback) {
+    this.update = function (data, callback) {
       // data file is updated to data (obj)
       // callback (optional) accepts the drive api response
       _gddf._getDataFileId(function(dataFileId) {
         $http.put(gDriveUploadURL + dataFileId + '?uploadType=media', data)
         .then(function (resp) {
-          respHandler.call(this, createResp);
-          if (typeof callback !== 'undefined') {
+          respHandler.call(this, resp);
+          if (typeof callback !== 'undefined')
             callback.call(this, resp);
-          }
         }, function (errResp) {
           errRespHandler.call(this, errResp);
         })
       })
+    };
+
+    this._clearCache = function () {
+      delete _gddf[dataFileIdKey];
+      delete _gddf[appfolderIdKey];
+    }
+
+    this._delete = function (callback) {
+      // callback (optional) accepts the drive api response
+      _gddf._getDataFileId(function (dataFileId) {
+        $http.delete(gDriveBaseURL + '/' + dataFileId)
+        .then(function (resp) {
+          respHandler.call(this, resp);
+          if (typeof callback !== 'undefined')
+            callback.call(this, resp);
+          _gddf._clearCache();
+        }, function (errResp) {
+          errRespHandler.call(this, errResp);
+        });
+      });
     };
   }])
   ;
--- extension/components/background/keychain.js
+++ extension/components/background/keychain.js
@@ -1,112 +1,24 @@
 (function () {
-  angular.module('keychain', ['commonServices'])
+  angular.module('keychain', ['commonServices', 'gDriveDataFileModule'])
 
-  .config(function ($httpProvider) {
-    chrome.identity.getAuthToken({interactive: true}, function (token) {
-      $httpProvider.defaults.headers.common.Authorization = 'Bearer ' + token;
-    })
-  })
+  .controller('GDriveCtrl', ['$scope', 'gDriveDataFile', function ($scope, gDriveDataFile) {
+    // $scope.gddf = gDriveDataFile;
 
-  .controller('GDriveCtrl', ['$scope', '$http', function ($scope, $http) {
-    /**
-     * Function to update data file
-     * Function to retrieve data file and load data from response
-     * Function to
-     *
-     */
-
-    var gDriveBaseURL = 'https://www.googleapis.com/drive/v2/files/';
-    var gDriveSimpleUploadURL = 'https://www.googleapis.com/upload/drive/v2/files?uploadType=media';
-    var gDriveMultipartUploadURL = 'https://www.googleapis.com/upload/drive/v2/files?uploadType=multipart';
-    var gDriveMetadataURL = 'https://www.googleapis.com/drive/v2/files';
-    var gDriveUploadURL = 'https://www.googleapis.com/upload/drive/v2/files/';
-
-    var simpleRespHandler = function (resp) {
-      console.log('resp received:', resp);
-      $scope.resp = resp;
-    };
-    var respErrorHandler = function (resp) {
-      console.log('error resp received:', resp);
-    };
-
-    $scope.getFileMetadata = function (fileId) {
-      fileId = fileId || 'appfolder';
-      var promise = $http.get(gDriveBaseURL + fileId);
-      promise.then(simpleRespHandler, respErrorHandler);
+    $scope.pre = '';
+    $scope.getData = function () {
+      gDriveDataFile.getData(function (data) {
+        $scope.pre = JSON.stringify(data);
+      });
     }
 
-    $scope.listFolder = function (folderId) {
-      folderId = folderId || 'appfolder';
-      var promise = $http.get(gDriveBaseURL + folderId + '/children');
-      promise.then(simpleRespHandler, respErrorHandler);
-    };
-
-    $scope.createDataFile = function () {
-      var metadata = {'title': 'chrome-keychain-app-data.json',
-                      'mimeType': 'application/json',
-                      'parents': [{'id': 'appfolder'}]}
-      var createPromise = $http.post(gDriveMetadataURL, metadata)
-      createPromise.then(simpleRespHandler, respErrorHandler);
+    $scope.update = function (k, v) {
+      data = {};
+      data[k] = v;
+      gDriveDataFile.update(data);
     }
 
-    $scope.updateDataFile = function (fileId) {
-      var data = {
-        'holy': 'fucking shit'
-      }
-      var updatePromise = $http.put(gDriveUploadURL + fileId + '?uploadType=media', data);
-      updatePromise.then(simpleRespHandler, respErrorHandler);
-    }
-
-    $scope.deleteDataFile = function (fileId) {
-      var deletePromise = $http.delete(gDriveBaseURL + fileId);
-      deletePromise.then(simpleRespHandler, respErrorHandler);
-    }
-
-    $scope.multipartCreateDataFile = function () {
-      // the multipart create functionality is probably not preferred.
-      // although it is slower to create the file with metadata in one request
-      // then update the file in a separate request, it is much simpler
-
-      var boundary = '-------314159265358979323846';
-      var delimiter = "\r\n--" + boundary + "\r\n";
-      var close_delim = "\r\n--" + boundary + "--";
-      var contentType = 'application/json';
-
-      var metadata = {'title': 'chrome-keychain-app-data.json',
-                      'mimeType': 'application/json',
-                      'parents': [{'id': 'appfolder'}]}
-
-      var data = {'some key': 'some value'};
-
-      var multipartRequestBody =
-          delimiter +
-          'Content-Type: application/json\r\n\r\n' +
-          JSON.stringify(metadata) +
-          delimiter +
-          'Content-Type: ' + contentType + '\r\n' +
-          '\r\n' +
-          JSON.stringify(data) +
-          close_delim;
-
-      var config = {
-        'headers': {
-          'Content-Type': 'multipart/mixed; boundary="' + boundary + '"'
-        }
-      }
-      var promise = $http.post(gDriveMultipartUploadURL,
-                               multipartRequestBody,
-                               config);
-      promise.then(simpleRespHandler, respErrorHandler);
-    }
-
-    $scope.getDataFile = function () {
-      $http.get(gDriveBaseURL + 'appfolder/children').then(
-        function (resp) {
-          var filePromise = $http.get(resp.data.items[0].childLink + '?alt=media');
-          filePromise.then(simpleRespHandler, respErrorHandler);
-        },
-        respErrorHandler
-      )
+    $scope.delete = function () {
+      gDriveDataFile._delete();
     }
   }])
 
